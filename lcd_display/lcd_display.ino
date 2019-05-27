37997b247db82706cd64c1520399691b048d395e
jaymundra <39671837+jaymundra@users.noreply.github.com>
2019-05-27 19:10:35 +0530

    Add files via upload

---
+++ lcd_display/lcd_display.ino
@@ -0,0 +1,14 @@
+String incoming;
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{ 
+
+  while(Serial.available()){
+  incoming = Serial.readString();
+  Serial.println(incoming);
+}}
